76337c073b4c8f2b00c197083e8f763083f0c81a
xuninc <4990604+xuninc@users.noreply.github.com>
2026-04-13 00:23:54 -0500

    Fix Afterglow preset colors and migrate legacy plum theme

--- Utils/YTAGUserDefaults.m
+++ Utils/YTAGUserDefaults.m
@@ -1,12 +1,54 @@
 #import "YTAGUserDefaults.h"
+#import <UIKit/UIKit.h>
 
 @implementation YTAGUserDefaults
 
 static NSString *const kDefaultsSuiteName = @"com.dvntm.ytafterglow";
 static NSString *const kActiveTabsKey = @"activeTabs";
 static NSString *const kStartupTabKey = @"startupTab";
+static NSString *const kThemeMigrationVersionKey = @"themePresetMigrationVersion";
 static const NSUInteger kMinimumActiveTabsCount = 2;
 static const NSUInteger kMaximumActiveTabsCount = 6;
+static const NSInteger kCurrentThemeMigrationVersion = 1;
+
+static NSData *YTAGArchiveColor(UIColor *color) {
+    return [NSKeyedArchiver archivedDataWithRootObject:color requiringSecureCoding:NO error:nil];
+}
+
+static BOOL YTAGThemeKeyMatchesColor(NSUserDefaults *defaults, NSString *key, UIColor *expectedColor) {
+    NSData *stored = [defaults objectForKey:key];
+    NSData *expected = YTAGArchiveColor(expectedColor);
+    return (stored && expected && [stored isEqualToData:expected]);
+}
+
+static BOOL YTAGThemeMatchesLegacyAfterglowPreset(NSUserDefaults *defaults) {
+    return
+        YTAGThemeKeyMatchesColor(defaults, @"theme_overlayButtons", [UIColor colorWithRed:0.99 green:0.86 blue:0.78 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_tabBarIcons", [UIColor colorWithRed:0.99 green:0.57 blue:0.39 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_seekBar", [UIColor colorWithRed:0.96 green:0.42 blue:0.28 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_background", [UIColor colorWithRed:0.11 green:0.06 blue:0.10 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_textPrimary", [UIColor colorWithRed:0.99 green:0.94 blue:0.90 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_textSecondary", [UIColor colorWithRed:0.72 green:0.61 blue:0.65 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_navBar", [UIColor colorWithRed:0.17 green:0.08 blue:0.12 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_accent", [UIColor colorWithRed:0.93 green:0.36 blue:0.26 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_gradientStart", [UIColor colorWithRed:0.18 green:0.07 blue:0.12 alpha:1.0]) &&
+        YTAGThemeKeyMatchesColor(defaults, @"theme_gradientEnd", [UIColor colorWithRed:0.39 green:0.12 blue:0.15 alpha:1.0]) &&
+        [defaults boolForKey:@"theme_glowEnabled"];
+}
+
+static void YTAGApplyUpdatedAfterglowPreset(NSUserDefaults *defaults) {
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:1.00 green:0.90 blue:0.78 alpha:1.0]) forKey:@"theme_overlayButtons"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.99 green:0.63 blue:0.32 alpha:1.0]) forKey:@"theme_tabBarIcons"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.97 green:0.49 blue:0.21 alpha:1.0]) forKey:@"theme_seekBar"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.07 green:0.07 blue:0.09 alpha:1.0]) forKey:@"theme_background"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.98 green:0.95 blue:0.90 alpha:1.0]) forKey:@"theme_textPrimary"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.70 green:0.62 blue:0.54 alpha:1.0]) forKey:@"theme_textSecondary"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.12 green:0.11 blue:0.12 alpha:1.0]) forKey:@"theme_navBar"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.94 green:0.44 blue:0.18 alpha:1.0]) forKey:@"theme_accent"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.16 green:0.15 blue:0.17 alpha:1.0]) forKey:@"theme_gradientStart"];
+    [defaults setObject:YTAGArchiveColor([UIColor colorWithRed:0.34 green:0.19 blue:0.11 alpha:1.0]) forKey:@"theme_gradientEnd"];
+    [defaults setBool:YES forKey:@"theme_glowEnabled"];
+}
 
 static NSArray<NSString *> *YTAGAllowedTabs(void) {
     return @[@"FEwhat_to_watch", @"FEshorts", @"FEsubscriptions", @"FElibrary", @"FEexplore", @"FEhistory", @"VLWL", @"FEpost_home", @"FEuploads"];
@@ -19,6 +61,7 @@ static NSArray<NSString *> *YTAGAllowedTabs(void) {
     dispatch_once(&onceToken, ^{
         defaults = [[self alloc] initWithSuiteName:kDefaultsSuiteName];
         [defaults registerDefaults];
+        [defaults migrateThemePresetsIfNeeded];
     });
 
     return defaults;
@@ -99,6 +142,17 @@ static NSArray<NSString *> *YTAGAllowedTabs(void) {
         kStartupTabKey: @"FEwhat_to_watch",
         @"frostedPivot": @YES
     }];
+}
+
+- (void)migrateThemePresetsIfNeeded {
+    NSInteger recordedVersion = [self integerForKey:kThemeMigrationVersionKey];
+    if (recordedVersion >= kCurrentThemeMigrationVersion) return;
+
+    if (YTAGThemeMatchesLegacyAfterglowPreset(self)) {
+        YTAGApplyUpdatedAfterglowPreset(self);
+    }
+
+    [self setInteger:kCurrentThemeMigrationVersion forKey:kThemeMigrationVersionKey];
 }
 
 + (NSArray<NSString *> *)defaultActiveTabs {
